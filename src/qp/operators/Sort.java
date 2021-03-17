cffbbd06cbdf45e54c26e7adc08dd183f892aa9f
drake25122000 <drake251@comp.nus.edu.sg>
2021-03-18 02:44:21 +0800

    Upgrade Sort algorithm

--- src/qp/operators/Sort.java
+++ src/qp/operators/Sort.java
@@ -16,8 +16,12 @@ public class Sort extends Operator {
     ArrayList<Integer> attIndexList;    // List of attribute index to be sort
     ArrayList<Attribute> attList;       // List of attribute to be sort
     static int filenum = 0;             // To get unique filenum for this operation
+    static int outfilenum = 0;          // Pointer for the outputfile num
+    static int iteration = 0;           // Pointer for the iteration
+    static int maxNumTuples = 0;        // Max number of tuple in sorted run after merging
     ObjectInputStream in;               // File pointer to the materialized file
     ObjectOutputStream out;             // File pointer to the materialized file
+    TupleReader tr;                     // tuple reader
     int numSortedRuns;                  // Number of sorted runs
     String rfname;                      // The file name where the right table is materialized
     SortedRun finalSortedRun;           // Final single sorted run from out
@@ -94,151 +98,122 @@ public class Sort extends Operator {
         // Initialize arraylist of tuples to be insert to SortedRuns
         ArrayList<Tuple> tempTuples = new ArrayList<>();
 
-        try {
+
+        System.out.println("_________");
 
             // Generate sorted runs
-            while ((temp = base.next()) != null) {
-                // Initialize target file name
-                rfname = "Stemp-" + String.valueOf(filenum);
-                out = new ObjectOutputStream(new FileOutputStream(rfname));
-                //System.out.println(temp.size());
-
-                for (int k = 0; k < numOfBuff; k++) {
-                    for (int p = 0; p < temp.size(); p++) {
-                        //System.out.println(temp.get(p));
-                        tempTuples.add(temp.get(p));
-                    }
+        while ((temp = base.next()) != null) {
+            // Initialize target file name
+            rfname = "Stemp-" + String.valueOf(iteration) + "-" + String.valueOf(filenum);
+            //System.out.println(temp.size());
 
 
-                    if ( k < numOfBuff - 1 ) {
-                        if ((temp = base.next()) == null) break;
-                    }
-                }
-                //System.out.println("Count: " + count);
-
-                Collections.sort(tempTuples, (tup1, tup2) -> compareTuples(tup1, tup2));
-
-                TupleWriter tw = new TupleWriter(rfname, tempTuples.size() * tuplesize);
-                tw.open();
-
-                for (int i = 0 ; i < tempTuples.size() ; i++) {
-                    tw.next(tempTuples.get(i));
+            for (int k = 0; k < numOfBuff; k++) {
+                for (int p = 0; p < temp.size(); p++) {
+                    //System.out.println(temp.get(p));
+                    tempTuples.add(temp.get(p));
                 }
 
-                tw.close();
-                //out.writeObject(new SortedRun(tempTuples));
-                numSortedRuns++;
-                filenum++;
-                tempTuples = new ArrayList<>();
 
+                if ( k < numOfBuff - 1 ) {
+                    if ((temp = base.next()) == null) break;
+                }
+            }
+            //System.out.println("Count: " + count);
+
+            // In memory sort
+            Collections.sort(tempTuples, (tup1, tup2) -> compareTuples(tup1, tup2));
+
+            // Update number of max tuples after merging
+            maxNumTuples = tempTuples.size() > maxNumTuples ? tempTuples.size() : maxNumTuples;
+
+            /*for (int l = 0 ; l < tempTuples.size() ; l++) {
+                System.out.println(tempTuples.get(l));
+            }*/
+
+            /*System.out.println("--____----_____--");*/
+
+            // Start tuple writer
+            TupleWriter tw = new TupleWriter(rfname, tempTuples.size());
+            tw.open();
+
+            //Write tuple to file
+            for (int i = 0 ; i < tempTuples.size() ; i++) {
+                tw.next(tempTuples.get(i));
             }
 
-            out.close();
-        } catch (IOException io) {
-            System.out.println("Sort: Error writing to temporary file");
-            return false;
+
+            tw.close();
+            //out.writeObject(new SortedRun(tempTuples));
+            numSortedRuns++;
+            filenum++;
+            tempTuples = new ArrayList<>();
+
         }
+
 
         filenum = 0;
-        try {
-            // Check whether the merge process still produce sorted runs more than 1
-            while (numSortedRuns > 1) {
 
-                rfname = "Stemp-" + String.valueOf(filenum);
-                // To initialize Object Input Stream everytime after merging process
-                in = new ObjectInputStream(new FileInputStream(rfname));
+        // To check if number of sorted runs greater than 1
+        while (numSortedRuns > 1) {
 
-                // To track number of sorted runs produced by the merging process
-                numSortedRuns = 0;
+            // To pointer for sorted runs that have been merged
+            int countSortedRuns = 0;
 
-                // Temporary variable for the sorted run
-                SortedRun tempSr;
+            // Temporary variable for number of sorted runs
+            int tempNumSortedRuns = numSortedRuns;
 
-                // Arraylist of sorted runs that is going to be sorted
-                ArrayList<SortedRun> srToMerge = new ArrayList<>();
-
-                //filenum++;
-                filenum = filenum == 0 ? 1 : 0;
-
-                rfname = "Stemp-" + String.valueOf(filenum);
-                out = new ObjectOutputStream(new FileOutputStream(rfname));
-                try {
-                    // Retrieve sorted runs from rfname file
-                    tempSr = (SortedRun) in.readObject();
-                    while (tempSr != null) {
-
-                        // To count sorted runs obtain from file
-                        int count = 0;
-
-                        // Check whether count is smaller than numOfBuff - 1 and tempSr is not null
-                        while (count < numOfBuff - 1 && tempSr != null) {
-                            //System.out.println(tempSr);
-                            srToMerge.add(tempSr);
-                            count++;
-                            tempSr = (SortedRun) in.readObject();
-                        }
-
-                        numSortedRuns++;
-                        mergeRuns(srToMerge);
+            // Reinitiate numsorted runs to 0
+            numSortedRuns = 0;
+            while (countSortedRuns < tempNumSortedRuns) {
+                ArrayList<String> indexes = new ArrayList<>();
+                for (int i = 0 ; i < numOfBuff - 1 ; i++) {
+                    if (countSortedRuns == tempNumSortedRuns) {
+                        break;
+                    } else {
+                        // To add sorted run file name to the list
+                        rfname = "Stemp-" + String.valueOf(iteration) + "-" + String.valueOf(countSortedRuns);
+                        indexes.add(rfname);
+                        // To count number of sorted runs that are merged in this process
+                        countSortedRuns++;
                     }
-                } catch (EOFException e) {
-                    try {
-                        numSortedRuns++;
-                        mergeRuns(srToMerge);
-                        in.close();
-                    } catch (IOException io) {
-                        System.out.println("Sort: Error in reading temporary file");
-                    }
-                } catch (IOException io) {
-                    System.out.println("Sort: Error in reading temporary file");
                 }
-                out.close();
+                //System.out.println(indexes);
+                numSortedRuns++;
+                mergeRuns(indexes);
 
+                // Delete files generated in previous iteration
+                for (String index : indexes) {
+                    File file = new File(index);
+                    file.delete();
+                }
             }
-        } catch (EOFException e) {
-            try {
-                in.close();
-            } catch (IOException io) {
-                System.out.println("Sort: Error in reading temporary file");
-            }
-        } catch (ClassNotFoundException c) {
-            System.out.println("Sort: Error in deserialising temporary file ");
-            System.exit(1);
-        } catch (IOException io) {
-            System.out.println("Sort: Error in reading temporary file");
-            System.exit(1);
+            // Increment iteration
+            iteration++;
+
+            // Reset oufilenum for new iteration
+            outfilenum = 0;
+
         }
 
-        filenum = filenum == 0 ? 1 : 0;
-        //filenum--;
-        filenum = filenum == 0 ? 1 : 0;
-        rfname = "Stemp-" + String.valueOf(filenum);
-        try {
-            // To get the final sorted runs from the file
-            in = new ObjectInputStream(new FileInputStream(rfname));
-            finalSortedRun = (SortedRun) in.readObject();
-            System.out.println(finalSortedRun.size());
-        } catch (EOFException e) {
-            try {
-                in.close();
-            } catch (IOException io) {
-                System.out.println("Sort: Error in reading temporary file");
-            }
-        } catch (ClassNotFoundException c) {
-            System.out.println("Sort: Error in deserialising temporary file ");
-            System.exit(1);
-        } catch (IOException io) {
-            System.out.println("Sort: Error in reading temporary file");
-            System.exit(1);
-        }
+        // Reset filenum for new iteration
+        filenum = 0;
+
+        // Update rf name to the latest sorted run file
+        rfname = "Stemp-" + String.valueOf(iteration) + "-" + String.valueOf(filenum);
+
+        // Open a new sorted run file
+        tr = new TupleReader(rfname, batchsize);
+        tr.open();
+
         return true;
 
     }
 
-    public boolean isOverMergeProcess (ArrayList<SortedRun> sortedruns) {
+    public boolean isOverMergeProcess (ArrayList<Batch> batches) {
 
-        for (int i = 0; i < sortedruns.size() ; i++) {
-            if (!sortedruns.get(i).isEmpty()) {
+        for (int i = 0; i < batches.size() ; i++) {
+            if (!batches.get(i).isEmpty()) {
                 return false;
             }
         }
@@ -246,8 +221,99 @@ public class Sort extends Operator {
         return true;
     }
 
-   
-    public void mergeRuns(ArrayList<SortedRun> sortedruns) {
+    public void mergeRuns(ArrayList<String> indexes) {
+        //System.out.println(indexes);
+
+        // Initiate number of buffer
+        ArrayList<Batch> batches = new ArrayList<>();
+
+        // Initiate number of tuple reader
+        ArrayList<TupleReader> readers = new ArrayList<>();
+
+        // Initiate number of out batch after merge
+        Batch tempOutBatch = new Batch(batchsize);
+
+        TupleWriter tw = new TupleWriter("Stemp-" + String.valueOf(iteration + 1) + "-" + String.valueOf(outfilenum), maxNumTuples);
+
+        tw.open();
+
+        // Pointer to which batch has the smallest value
+        int minimum = 0;
+
+        // Count how many tuple merged
+        int count = 0;
+
+        //Initialize batches and readers
+        for (int i = 0 ; i < indexes.size() ; i++) {
+            TupleReader tempReader = new TupleReader(indexes.get(i), batchsize);
+            tempReader.open();
+            Batch temp = new Batch(batchsize);
+
+            while (!temp.isFull()){
+
+                if ( tempReader.isEOF() ) {
+                    break;
+                }
+
+                temp.add(tempReader.next());
+            }
+
+            /*for (int k = 0 ; k < temp.size(); k++) {
+                System.out.println(temp.get(k));
+            }*/
+            batches.add(temp);
+            readers.add(tempReader);
+        }
+
+        // Check if all of the tuple in the sorted runs have been polled
+        while (!isOverMergeProcess(batches)) {
+            // Check if sorted run with index minimum is already empty
+            while (batches.get(minimum).isEmpty()) {
+                minimum++;
+            }
+
+            // Get the smallest tuple from the first element of every sorted runs
+            for (int i = minimum + 1 ; i < batches.size() ; i++) {
+                if (!batches.get(i).isEmpty() && compareTuples(batches
+                        .get(i).get(0), batches.get(minimum).get(0)) < 0 ) {
+                    minimum = i;
+                }
+            }
+
+            Tuple tempTuple = batches.get(minimum).poll();
+            tempOutBatch.add(tempTuple);
+
+            if (tempOutBatch.isFull()) {
+                while (!tempOutBatch.isEmpty()) {
+                    //System.out.println(tempOutBatch.get(0));
+                    tw.next(tempOutBatch.poll());
+                    count++;
+                }
+            }
+
+            if ( !readers.get(minimum).isEOF() ) {
+                batches.get(minimum).add(readers.get(minimum).next());
+            }
+
+            minimum = 0;
+        }
+
+        while (!tempOutBatch.isEmpty()) {
+            //System.out.println(tempOutBatch.get(0));
+            tw.next(tempOutBatch.poll());
+            count++;
+        }
+
+        //System.out.println("-----");
+
+        maxNumTuples = count > maxNumTuples ? count : maxNumTuples;
+
+        tw.close();
+        outfilenum++;
+
+    }
+
+/*    public void mergeRuns(ArrayList<SortedRun> sortedruns) {
 
         SortedRun temp = new SortedRun(new ArrayList<Tuple>());
         int minimum = 0;
@@ -278,7 +344,7 @@ public class Sort extends Operator {
             System.out.println("Sort: Error writing to temporary file");
             System.exit(1);
         }
-    }
+    }*/
 
     /**
      * returns a batch of tuples that satisfies the
@@ -286,13 +352,10 @@ public class Sort extends Operator {
      * * NOTE: This operation is performed on the fly
      **/
     public Batch next() {
-        int i = 0;
 
-        if (finalSortedRun.isEmpty()) {
-            close();
+        if (tr.isEOF()) {
             return null;
         }
-
 
         /** An output buffer is initiated **/
         outbatch = new Batch(batchsize);
@@ -300,9 +363,10 @@ public class Sort extends Operator {
         /** keep on checking the incoming pages until
          ** the output buffer is full & the final sorted run is empty
          **/
-        while (!outbatch.isFull() && !finalSortedRun.isEmpty()) {
-            outbatch.add(finalSortedRun.poll(0));
+        while (!outbatch.isFull() && !tr.isEOF()) {
+            outbatch.add(tr.next());
         }
+
 
         return outbatch;
 
